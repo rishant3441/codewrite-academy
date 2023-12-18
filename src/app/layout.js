aa9129fe96f707823ee44a8e4081e4ea437ffc1c
Rishan Thangaraj <83434519+rishant3441@users.noreply.github.com>
2023-12-18 14:14:30 +0000

    Taking website down until release

--- src/app/layout.js
+++ src/app/layout.js
@@ -45,11 +45,12 @@ export default function RootLayout({ children }) {
       <head>
         <Favicon />
       </head>
-      <body className={noto_sans.className}>
+      {/*<body className={noto_sans.className}>
         <Appbar />
         {children}
         <Footer />
-      </body>
+  </body>*/}
+      <body><div><h1>Coming soon!</h1></div></body>
     </html>
   )
 }
