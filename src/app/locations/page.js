912a0dcf1f73e4f5e22f55a18bdfe925ba2f492c
Rishan Thangaraj <rishanthangaraj@gmail.com>
2024-12-28 17:51:25 -0500

    Change carousel

--- src/app/locations/page.js
+++ src/app/locations/page.js
@@ -1,3 +1,4 @@
+'use client'
 import {
     MDBCarousel,
     MDBCarouselItem,
@@ -9,6 +10,7 @@ import {
     MDBBtn
 } from '@/lib/mdb-react-ui-kit';
 
+
 import Image from 'next/image'
 import styles from './../page.module.css'
 import rishan_and_kid from '../../../public/BGCDelray/rishan_and_kid.jpg'
@@ -18,37 +20,65 @@ import zoe_and_kid from '../../../public/BGCDelray/zoe_and_kid.jpg'
 import zoe_working_on_building_gamepad from '../../../public/BGCDelray/zoe_working_on_building_gamepad.jpg'
 import zoe_working_on_game_2 from '../../../public/BGCDelray/zoe_working_on_game_2.jpg'
 import zoe_working_on_game from '../../../public/BGCDelray/zoe_working_on_game.jpg'
-
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css';
 export default function Locations()
 {
+    const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 2,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 464 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
     return (
         <div className="pt-5 pb-5">
             <h1 className="text-center text-blue-400"> Boys and Girls Club Delray Beach</h1>
-            <MDBContainer className="flex justify-center">
-                <MDBCarousel showControls showIndicators interval={7000} className="w-3/5">
-                    <MDBCarouselItem itemId={1} className="active" isActive>
-                        <Image src={zoe_and_kid} alt="Zoe working with a kid" className="d-block w-auto"/>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId={2}>
-                        <Image src={rishan_and_kids_playing_game} alt="Rishan and kids playing their games" className="d-block w-auto"/>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId={3}>
-                        <Image src={zoe_working_on_building_gamepad} alt="Zoe working with building a small gamepad" className="d-block w-auto"/>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId={4}>
-                        <Image src={rishan_working_on_game} alt="Rishan working on game" className="d-block w-auto"/>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId={5}>
-                        <Image src={zoe_working_on_game} alt="Zoe working on game" className="d-block w-auto"/>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId={6}>
-                        <Image src={rishan_and_kid} alt="Rishan and Kid" className="d-block w-auto"/>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId={7}>
-                        <Image src={zoe_working_on_game_2} alt="Zoe working on game" className="d-block w-auto"/>
-                    </MDBCarouselItem>
-                   
-                </MDBCarousel>
+            <MDBContainer className="w-11/12 flex h-auto justify-content-center max-w-screen-2xl">
+                <Carousel
+                    additionalTransfrom={0}
+                    arrows
+                    autoPlaySpeed={3000}
+                    centerMode={false}
+                    containerClass="w-full"
+                    draggable
+                    focusOnSelect={false}
+                    infinite
+                    keyBoardControl
+                    minimumTouchDrag={80}
+                    pauseOnHover
+                    renderArrowsWhenDisabled={false}
+                    renderButtonGroupOutside={false}
+                    renderDotsOutside={false}
+                    responsive={responsive}
+                    rewind={false}
+                    rewindWithAnimation={false}
+                    rtl={false}
+                    shouldResetAutoplay
+                    showDots
+                    autoPlay={true}
+                    slidesToSlide={1}
+                    swipeable
+                    ssr={false}
+                    >
+                    <Image src={zoe_working_on_game_2} alt="Zoe working on game" className="d-block w-auto px-2"/>
+                    <Image src={zoe_and_kid} alt="Zoe working with a kid" className="d-block w-auto px-2"/>
+                    <Image src={rishan_and_kids_playing_game} alt="Rishan and kids playing their games" className="d-block w-auto px-2"/>
+                    <Image src={zoe_working_on_building_gamepad} alt="Zoe working with building a small gamepad" className="d-block w-auto px-2"/>
+                    <Image src={rishan_working_on_game} alt="Rishan working on game" className="d-block w-auto px-2"/>
+                    <Image src={zoe_working_on_game} alt="Zoe working on game" className="d-block w-auto px-2"/>
+                    <Image src={rishan_and_kid} alt="Rishan and Kid" className="d-block w-auto px-2"/>
+                </Carousel>
             </MDBContainer>
         </div>
     )
