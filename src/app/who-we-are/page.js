5b5421a56b576bdd9418c32811d3048a7e4a7e1f
Rishan Thangaraj <rishanthangaraj@gmail.com>
2025-01-12 00:02:39 -0500

    New who-we-are page

--- src/app/who-we-are/page.js
+++ src/app/who-we-are/page.js
@@ -12,6 +12,8 @@ import React from 'react';
 
 import Image from 'next/image';
 
+import pic from '../../../public/who-we-are-new.jpg'
+
 function WhoWeAre()
 {
     return (
@@ -42,18 +44,18 @@ function WhoWeAre()
                         </MDBRow>
                         <MDBRow className="mb-3 text-lg text-zinc-800">
                             <MDBCol>
-                                <p>Welcome to CodeWrite Academy, a student-led volunteer organization dedicated to teaching programming to younger students. We believe that coding is a vital skill for the future and aim to inspire and empower the next generation of programmers. </p>
+                                <p>Welcome to CodeWrite Academy, a student-led volunteer organization dedicated to teach the next generation of programmers. We believe that programming is an essential skill and we hope to inspire and empower the future. </p>
 
-                                <p>Our diverse team of talented mentors is passionate about making programming accessible and enjoyable for all. Through interactive workshops, coding challenges, and hands-on projects, we create a supportive learning environment. Our experienced mentors guide students, fostering collaboration and inclusivity. </p>
+                                <p>Our diverse team of talented volunteers is passionate in helping make programming accessible and enjoyable for all. Through our fun and interactive lessons, challenges, and projects, kids are taught in a way that'll stick. Our experienced mentors guide students through the process, ensuring that they understand the content through and through.</p>
 
-                                <p>As students ourselves, we understand the issues with current teaching methods and aim to create a curriculum that is fun and engaging for students, helping them achieve their dreams, in the simplest way possible.</p>
+                                <p>Being students ourselves, we understand the problems with current teaching methods and aim to curate a curriculum that is fun and engaging for our students, helping them become the next brightest students of their generation. </p>
                             </MDBCol>
                         </MDBRow>
 
                     </div>
 
                     <div className="col-12 col-md-6 p-3">
-                        <Image src="/StockPhoto_square.jpg" width="500" height="500" className="mx-auto"></Image>
+                        <Image src={pic} alt="Kids leading kids" className="w-[510px] mx-auto"></Image>
                     </div>
                 </MDBRow>
             </MDBContainer>
