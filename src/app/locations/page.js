8543ee90b8e693e2f0d04a1523d62832477900a0
Rishan Thangaraj <rishanthangaraj@gmail.com>
2025-01-14 20:20:17 -0500

    Add header to locations page

--- src/app/locations/page.js
+++ src/app/locations/page.js
@@ -55,81 +55,97 @@ export default function Locations()
   }
 };
     return (
-        <div className="pt-5 pb-5">
-            <h1 className="text-center text-blue-400"> Boys and Girls Club Delray Beach</h1>
-            <MDBContainer className="w-11/12 flex h-auto justify-content-center max-w-screen-2xl">
-                <Carousel
-                    additionalTransfrom={0}
-                    arrows
-                    autoPlaySpeed={3000}
-                    centerMode={false}
-                    containerClass="w-full"
-                    draggable
-                    focusOnSelect={false}
-                    infinite
-                    keyBoardControl
-                    minimumTouchDrag={80}
-                    pauseOnHover
-                    renderArrowsWhenDisabled={false}
-                    renderButtonGroupOutside={false}
-                    renderDotsOutside={false}
-                    responsive={responsive}
-                    rewind={false}
-                    rewindWithAnimation={false}
-                    rtl={false}
-                    shouldResetAutoplay
-                    showDots
-                    autoPlay={true}
-                    slidesToSlide={1}
-                    swipeable
-                    ssr={false}
-                    >
-                    <Image src={zoe_working_on_game_2} alt="Zoe working on game" className="d-block w-auto px-2"/>
-                    <Image src={zoe_and_kid} alt="Zoe working with a kid" className="d-block w-auto px-2"/>
-                    <Image src={rishan_and_kids_playing_game} alt="Rishan and kids playing their games" className="d-block w-auto px-2"/>
-                    <Image src={zoe_working_on_building_gamepad} alt="Zoe working with building a small gamepad" className="d-block w-auto px-2"/>
-                    <Image src={rishan_working_on_game} alt="Rishan working on game" className="d-block w-auto px-2"/>
-                    <Image src={zoe_working_on_game} alt="Zoe working on game" className="d-block w-auto px-2"/>
-                    <Image src={rishan_and_kid} alt="Rishan and Kid" className="d-block w-auto px-2"/>
-                </Carousel>
-            </MDBContainer>
-            <h1 className="pt-5 text-center text-blue-400"> Boys and Girls Club Boca Raton</h1>
-            <MDBContainer className="w-11/12 flex h-auto justify-content-center max-w-screen-2xl">
-                <Carousel
-                    additionalTransfrom={0}
-                    arrows
-                    autoPlaySpeed={3000}
-                    centerMode={false}
-                    containerClass="w-full"
-                    draggable
-                    focusOnSelect={false}
-                    infinite
-                    keyBoardControl
-                    minimumTouchDrag={80}
-                    pauseOnHover
-                    renderArrowsWhenDisabled={false}
-                    renderButtonGroupOutside={false}
-                    renderDotsOutside={false}
-                    responsive={responsive}
-                    rewind={false}
-                    rewindWithAnimation={false}
-                    rtl={false}
-                    shouldResetAutoplay
-                    showDots
-                    autoPlay={true}
-                    slidesToSlide={1}
-                    swipeable
-                    ssr={false}
-                    >
-                    <Image src={boy_playing_game} alt="Boy plays game" className="d-block w-auto px-2"/>
-                    <Image src={zoe_teaching_on_board} alt="Zoe teaches kid on smartboard" className="d-block w-auto px-2"/>
-                    <Image src={girls_playing_game} alt="2 girls playing game" className="d-block w-auto px-2"/>
-                    <Image src={kids_working1} alt="Zoe teaching girl while others work" className="d-block w-auto px-2"/>
-                    <Image src={boys_playing_game} alt="Boys plays game" className="d-block w-auto px-2"/>
-                    <Image src={kids_working2} alt="Zoe teaching girl while others work" className="d-block w-auto px-2"/>
-                    <Image src={more_girls_playing_games} alt="3 girls playing game" className="d-block w-auto px-2"/>
-                </Carousel>
-            </MDBContainer>
+        <div>
+            <header>
+                <div
+                    className='p-5 text-center bg-image'
+                    style={{ backgroundImage: "url('/Empty_Border.png')", height: '400px' }}
+                >
+                    <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.0)' }}>
+                        <div className='d-flex justify-content-center align-items-center h-100'>
+                            <div className='text-black'>
+                                <h1 className='mb-3 font-bold text-6xl'>Our Locations</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <div className="pt-5 pb-5">
+                <h1 className="text-center text-blue-400"> Boys and Girls Club Delray Beach</h1>
+                <MDBContainer className="w-11/12 flex h-auto justify-content-center max-w-screen-2xl">
+                    <Carousel
+                        additionalTransfrom={0}
+                        arrows
+                        autoPlaySpeed={3000}
+                        centerMode={false}
+                        containerClass="w-full"
+                        draggable
+                        focusOnSelect={false}
+                        infinite
+                        keyBoardControl
+                        minimumTouchDrag={80}
+                        pauseOnHover
+                        renderArrowsWhenDisabled={false}
+                        renderButtonGroupOutside={false}
+                        renderDotsOutside={false}
+                        responsive={responsive}
+                        rewind={false}
+                        rewindWithAnimation={false}
+                        rtl={false}
+                        shouldResetAutoplay
+                        showDots
+                        autoPlay={true}
+                        slidesToSlide={1}
+                        swipeable
+                        ssr={false}
+                        >
+                        <Image src={zoe_working_on_game_2} alt="Zoe working on game" className="d-block w-auto px-2"/>
+                        <Image src={zoe_and_kid} alt="Zoe working with a kid" className="d-block w-auto px-2"/>
+                        <Image src={rishan_and_kids_playing_game} alt="Rishan and kids playing their games" className="d-block w-auto px-2"/>
+                        <Image src={zoe_working_on_building_gamepad} alt="Zoe working with building a small gamepad" className="d-block w-auto px-2"/>
+                        <Image src={rishan_working_on_game} alt="Rishan working on game" className="d-block w-auto px-2"/>
+                        <Image src={zoe_working_on_game} alt="Zoe working on game" className="d-block w-auto px-2"/>
+                        <Image src={rishan_and_kid} alt="Rishan and Kid" className="d-block w-auto px-2"/>
+                    </Carousel>
+                </MDBContainer>
+                <h1 className="pt-5 text-center text-blue-400"> Boys and Girls Club Boca Raton</h1>
+                <MDBContainer className="w-11/12 flex h-auto justify-content-center max-w-screen-2xl">
+                    <Carousel
+                        additionalTransfrom={0}
+                        arrows
+                        autoPlaySpeed={3000}
+                        centerMode={false}
+                        containerClass="w-full"
+                        draggable
+                        focusOnSelect={false}
+                        infinite
+                        keyBoardControl
+                        minimumTouchDrag={80}
+                        pauseOnHover
+                        renderArrowsWhenDisabled={false}
+                        renderButtonGroupOutside={false}
+                        renderDotsOutside={false}
+                        responsive={responsive}
+                        rewind={false}
+                        rewindWithAnimation={false}
+                        rtl={false}
+                        shouldResetAutoplay
+                        showDots
+                        autoPlay={true}
+                        slidesToSlide={1}
+                        swipeable
+                        ssr={false}
+                        >
+                        <Image src={boy_playing_game} alt="Boy plays game" className="d-block w-auto px-2"/>
+                        <Image src={zoe_teaching_on_board} alt="Zoe teaches kid on smartboard" className="d-block w-auto px-2"/>
+                        <Image src={girls_playing_game} alt="2 girls playing game" className="d-block w-auto px-2"/>
+                        <Image src={kids_working1} alt="Zoe teaching girl while others work" className="d-block w-auto px-2"/>
+                        <Image src={boys_playing_game} alt="Boys plays game" className="d-block w-auto px-2"/>
+                        <Image src={kids_working2} alt="Zoe teaching girl while others work" className="d-block w-auto px-2"/>
+                        <Image src={more_girls_playing_games} alt="3 girls playing game" className="d-block w-auto px-2"/>
+                    </Carousel>
+                </MDBContainer>
+            </div>
         </div>
     )
 }
